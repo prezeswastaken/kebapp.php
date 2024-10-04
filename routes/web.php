b42ab16dcd779ef529117397836dc9ab38913851
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-10-04 15:33:58 +0200

    test github action again

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return response()->json(['message' => 'Thank you for using Laravel template for building Kebapp.php :)']);
-    return response()->json(['message' => 'Hello from github action!']);
+    return response()->json(['message' => 'Hello from github action again!']);
 });
