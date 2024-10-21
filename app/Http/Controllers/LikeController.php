58ea9616cbd63975cecb346194246400815b7e7c
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-10-21 15:08:23 +0200

    feat: admin messages for liking kebabs

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AdminLog;
 use App\Models\Kebab;
 use App\Models\User;
 use Illuminate\Container\Attributes\CurrentUser;
@@ -11,5 +12,22 @@ class LikeController extends Controller
     public function __invoke(Kebab $kebab, #[CurrentUser] User $user)
     {
         $user->likes()->toggle($kebab->id);
+
+        $user->load('likes');
+        $user->refresh();
+
+        $logMessage = match ($user->likes->where('id', $kebab->id)->first() != null) {
+            true => "Has liked $kebab->name",
+            false => "Removed like of $kebab->name",
+        };
+
+        AdminLog::create([
+            'user_name' => $user->name,
+            'method' => 'POST',
+            'action_name' => $logMessage,
+        ]);
+
+        return response()->json(['message' => $logMessage]);
+
     }
 }
