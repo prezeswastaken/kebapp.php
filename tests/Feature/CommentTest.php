9b1fe64bc8b8a25b3e2a9b3baf34f2abb8464a0f
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-10-21 17:42:09 +0200

    feat: tests for commenting

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\Comment;
 use App\Models\Kebab;
 use App\Models\User;
 use Illuminate\Contracts\Auth\Authenticatable;
@@ -28,5 +29,34 @@ class CommentTest extends TestCase
         /** @var User $user */
         $user->refresh();
         $this->assertNotNull($user->comments->where('kebab_id', 1)->first());
+    }
+
+    public function test_user_can_delete_their_comment(): void
+    {
+        /** @var Authenticatable $user */
+        $user = User::factory()->create();
+        $text = $this->faker()->text();
+        $response = $this->actingAs($user)->post('/api/comment/kebab/1', ['text' => $text]);
+        $response->assertStatus(200);
+        $commentId = $response->json('id');
+
+        $response = $this->actingAs($user)->delete("/api/comment/$commentId");
+        $response->assertNoContent();
+        $this->assertNull(Comment::find($commentId));
+    }
+
+    public function test_user_cant_delete_other_users_comment(): void
+    {
+        $otherUser = User::factory()->create();
+        $otherUser->comments()->create(['text' => 'random comment text...', 'kebab_id' => 1]);
+        $comment = $otherUser->comments->first();
+        $commentId = $comment->id;
+
+        /** @var Authenticatable $user */
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->delete("/api/comment/$commentId");
+
+        $response->assertStatus(403);
+        $this->assertDatabaseHas($comment);
     }
 }
