2719c74d14ea90c281ede0a5b26ae53a8546ccb3
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-10-22 16:41:12 +0200

    feat: filtering kebabs

--- app/Actions/GetSortedFilteredKebabsAction.php
+++ app/Actions/GetSortedFilteredKebabsAction.php
@@ -15,6 +15,43 @@ class GetSortedFilteredKebabsAction
     public function handle(KebabSortFilterParams $params): Builder
     {
         return Kebab::with(['openingHours', 'meatTypes', 'sauces', 'likes'])
+            ->when($params->meatTypes, function (Builder $query, array $meatTypes) {
+                $query->whereRelation('meatTypes', function ($q) use ($meatTypes) {
+                    $q->whereIn('name', $meatTypes);
+                });
+            })
+            ->when($params->sauces, function (Builder $query, array $sauces) {
+                $query->whereRelation('sauces', function ($q) use ($sauces) {
+                    $q->whereIn('name', $sauces);
+                });
+            })
+            ->when($params->statuses, function (Builder $query, array $statuses) {
+                $query->whereIn('status', $statuses);
+            })
+            ->when(isset($params->isKraft), function (Builder $query) use ($params) {
+                $query->where('is_kraft', $params->isKraft);
+            })
+            ->when(isset($params->isFoodTruck), function (Builder $query) use ($params) {
+                $query->where('is_food_truck', $params->isFoodTruck);
+            })
+            ->when(isset($params->hasGlovo), function (Builder $query) use ($params) {
+                $query->where('has_glovo', $params->hasGlovo);
+            })
+            ->when(isset($params->hasPyszne), function (Builder $query) use ($params) {
+                $query->where('has_pyszne', $params->hasPyszne);
+            })
+            ->when(isset($params->hasUberEats), function (Builder $query) use ($params) {
+                $query->where('has_ubereats', $params->hasUberEats);
+            })
+            ->when(isset($params->hasPhone), function (Builder $query) use ($params) {
+                $params->hasPhone ? $query->whereNotNull('phone_number') : $query->whereNull('phone_number');
+            })
+            ->when(isset($params->hasWebsite), function (Builder $query) use ($params) {
+                $params->hasWebsite ? $query->whereNotNull('website_link') : $query->whereNull('website_link');
+            })
+            ->when(isset($params->hasNetwork), function (Builder $query) use ($params) {
+                $params->hasNetwork ? $query->whereNotNull('network') : $query->whereNull('network');
+            })
             ->orderBy('id', 'desc');
     }
 }
--- app/ValueObjects/KebabSortFilterParams.php
+++ app/ValueObjects/KebabSortFilterParams.php
@@ -8,16 +8,75 @@ use Illuminate\Http\Request;
 
 class KebabSortFilterParams
 {
+    public readonly ?bool $isKraft;
+
+    public readonly ?bool $isFoodTruck;
+
+    public readonly ?bool $hasGlovo;
+
+    public readonly ?bool $hasPyszne;
+
+    public readonly ?bool $hasUberEats;
+
+    public readonly ?bool $hasPhone;
+
+    public readonly ?bool $hasWebsite;
+
+    public readonly ?bool $hasNetwork;
+
     private function __construct(
         public readonly ?array $meatTypes,
         public readonly ?array $sauces,
-    ) {}
+        public readonly ?array $statuses,
+        ?string $isKraft,
+        ?string $isFoodTruck,
+        ?string $hasGlovo,
+        ?string $hasPyszne,
+        ?string $hasUberEats,
+        ?string $hasPhone,
+        ?string $hasWebsite,
+        ?string $hasNetwork,
+    ) {
+        $this->isKraft = $this->parseBool($isKraft);
+        $this->isFoodTruck = $this->parseBool($isFoodTruck);
+        $this->hasGlovo = $this->parseBool($hasGlovo);
+        $this->hasPyszne = $this->parseBool($hasPyszne);
+        $this->hasUberEats = $this->parseBool($hasUberEats);
+        $this->hasPhone = $this->parseBool($hasPhone);
+        $this->hasWebsite = $this->parseBool($hasWebsite);
+        $this->hasNetwork = $this->parseBool($hasNetwork);
+    }
 
     public static function fromRequest(Request $request)
     {
         return new self(
             meatTypes: $request->has('meatTypes') ? explode(',', $request->get('meatTypes')) : null,
             sauces: $request->has('sauces') ? explode(',', $request->get('sauces')) : null,
+            statuses: $request->has('statuses') ? explode(',', $request->get('statuses')) : null,
+            isKraft: $request->get('isKraft'),
+            isFoodTruck: $request->get('isFoodTruck'),
+            hasGlovo: $request->get('hasGlovo'),
+            hasPyszne: $request->get('hasPyszne'),
+            hasUberEats: $request->get('hasUberEats'),
+            hasPhone: $request->get('hasPhone'),
+            hasWebsite: $request->get('hasWebsite'),
+            hasNetwork: $request->get('hasNetwork'),
         );
+    }
+
+    private function parseBool(?string $string): ?bool
+    {
+        if (! isset($string)) {
+            return null;
+        }
+        $positive = ['true',  '1'];
+        $negative = ['false', '2'];
+        if (in_array($string, $positive)) {
+            return true;
+        } elseif (in_array($string, $negative)) {
+            return false;
+        } else {
+            return null;
+        }
     }
 }
