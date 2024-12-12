3fee0374cbad515f8184eeeb38031c61e8686eeb
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-12-12 14:58:00 +0100

    added /docs endpoint

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
     // return response()->json(['message' => 'Thank you for using Laravel template for building Kebapp.php :)']);
     return response()->json(['message' => 'Hello from github action again!']);
 });
+
+Route::get('/docs', function () {
+    return redirect()->to('https://documenter.getpostman.com/view/29357733/2sAXxMftGJ#get-started-here');
+});
